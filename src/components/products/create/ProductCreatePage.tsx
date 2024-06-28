f27b6a1c7169ddaa407ca1d3b11c6f6cba3a5961
Ilyadelidon <Illadelidon95@gmail.com>
2024-06-28 20:20:12 +0300

    add functional create product

--- src/components/products/create/ProductCreatePage.tsx
+++ src/components/products/create/ProductCreatePage.tsx
@@ -3,262 +3,319 @@ import { PlusOutlined} from '@ant-design/icons';
 import type {UploadChangeParam} from 'antd/es/upload';
 import {useNavigate} from "react-router-dom";
 import TextArea from "antd/es/input/TextArea";
-import {ICategoryName, IProductCreate} from "../../types.ts";
+import {IBrandName, ICategoryName, IGenderName, IProductCreate, ISizeName, ISubCategoryName} from "../../types.ts";
 import http_common from "../../../http_common.ts";
 import {IUploadedFile} from "../../types.ts";
 import {useEffect, useState} from "react";
 
-const PostCreatePage = () => {
+const ProductCreatePage = () => {
     const [categories, setCategories] = useState<ICategoryName[]>([]);
-    const navigate = useNavigate();
+    const [brands, setBrands] = useState<IBrandName[]>([]);
+    const [sizes, setSizes] = useState<ISizeName[]>([]);
+    const [genders, setGenders] = useState<IGenderName[]>([]);
+    const [subcategories, setSubcategories] = useState<ISubCategoryName[]>([]);
+    const [filteredSubcategories, setFilteredSubcategories] = useState<ISubCategoryName[]>([]);
 
+    const navigate = useNavigate();
     const [form] = Form.useForm<IProductCreate>();
 
     useEffect(() => {
-        http_common.get<ICategoryName[]>("/api/categories/names")
-            .then(resp=> {
-                //console.log("list categories", resp.data);
-                setCategories(resp.data);
-            });
-    },[]);
+        fetchCategories();
+        fetchBrands();
+        fetchSizes();
+        fetchGenders();
+        fetchSubcategories(); // Додано виклик функції для завантаження підкатегорій
+    }, []);
+
+    const fetchCategories = async () => {
+        try {
+            const response = await http_common.get<ICategoryName[]>("/api/Dashboard/GetAllCategory");
+            setCategories(response.data);
+        } catch (error) {
+            console.error("Error fetching categories:", error);
+        }
+    };
+
+    const fetchBrands = async () => {
+        try {
+            const response = await http_common.get<IBrandName[]>("/api/Dashboard/GetAllBrand");
+            setBrands(response.data);
+        } catch (error) {
+            console.error("Error fetching brands:", error);
+        }
+    };
+
+    const fetchSizes = async () => {
+        try {
+            const response = await http_common.get<ISizeName[]>("/api/Dashboard/GetAllSizes");
+            setSizes(response.data);
+        } catch (error) {
+            console.error("Error fetching sizes:", error);
+        }
+    };
+
+    const fetchGenders = async () => {
+        try {
+            const response = await http_common.get<IGenderName[]>("/api/Dashboard/GetAllGenders");
+            setGenders(response.data);
+        } catch (error) {
+            console.error("Error fetching genders:", error);
+        }
+    };
+
+    const fetchSubcategories = async () => { // Додано функцію для завантаження підкатегорій
+        try {
+            const response = await http_common.get<ISubCategoryName[]>("/api/Dashboard/GetAllSubCategory");
+            setSubcategories(response.data);
+        } catch (error) {
+            console.error("Error fetching subcategories:", error);
+        }
+    };
+
+    const handleCategoryChange = (value: number) => {
+        const filtered = subcategories.filter(subcat => subcat.categoryId === value);
+        setFilteredSubcategories(filtered);
+    };
+
+    const categoriesData = categories?.map(item => ({ label: item.name, value: item.id }));
+    const brandsData = brands?.map(item => ({ label: item.name, value: item.id }));
+    const subcategoriesData = filteredSubcategories?.map(item => ({ label: item.name, value: item.id }));
 
     const onSubmit = async (values: IProductCreate) => {
-        console.log("data", values);
         try {
-            await http_common.post("/api/posts", values, {
+            await http_common.post("/api/Dashboard/CreateProduct", values, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
+
             });
             navigate("/");
+        } catch (ex) {
+            console.log("error>",values)
+            console.error("Exception creating product:", ex);
         }
-        catch(ex) {
-            console.log("Exception create category", ex);
-        }
-    }
-
-    const categoriesData = categories?.map(item => ({label: item.name, value: item.id}));
+    };
 
     return (
         <>
             <h1>Add Product</h1>
             <Row gutter={16}>
-                <Form form={form}
-                      onFinish={onSubmit}
-                      layout={"vertical"}
-                      style={{
-                          minWidth: '100%',
-                          display: 'flex',
-                          flexDirection: 'column',
-                          justifyContent: 'center',
-                          padding: 20,
-                      }}
+                <Form
+                    form={form}
+                    onFinish={onSubmit}
+                    layout={"vertical"}
+                    style={{
+                        minWidth: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        padding: 20,
+                    }}
                 >
                     <Form.Item
                         label="Name"
-                        name="name"
-                        htmlFor="name"
+                        name="Name"
+                        htmlFor="Name"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {min: 3, message: 'Name must have at least 3 symbols!'},
+                            { required: true, message: 'It is a required field!' },
+                            { min: 3, message: 'Name must have at least 3 characters!' },
                         ]}
                     >
-                        <Input autoComplete="name"/>
+                        <Input autoComplete="name" />
                     </Form.Item>
 
                     <Form.Item
                         label="Description"
-                        name="description"
-                        htmlFor="description"
+                        name="Description"
+                        htmlFor="Description"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {min: 10, message: 'Name must have at least 10 symbols!'},
+                            { required: true, message: 'It is a required field!' },
+                            { min: 10, message: 'Description must have at least 10 characters!' },
                         ]}
                     >
-                        <TextArea/>
+                        <TextArea />
                     </Form.Item>
 
                     <Form.Item
                         label="Category"
-                        name="category_id"
-                        htmlFor="category_id"
+                        name="CategoryId"
+                        htmlFor="CategoryId"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
+                            { required: true, message: 'It is a required field!' },
                         ]}
                     >
                         <Select
                             placeholder="Select the category: "
                             options={categoriesData}
+                            onChange={handleCategoryChange}
                         />
                     </Form.Item>
 
                     <Form.Item
                         label="SubCategory"
-                        name="Subcategory_id"
-                        htmlFor="Subcategory_id"
+                        name="SubCategoryId"
+                        htmlFor="SubCategoryId"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
+                            { required: true, message: 'It is a required field!' },
                         ]}
                     >
                         <Select
                             placeholder="Select the SubCategory: "
-                            options={categoriesData}
+                            options={subcategoriesData}
                         />
                     </Form.Item>
 
                     <Form.Item
                         label="Price"
-                        name="price"
-                        htmlFor="price"
+                        name="Price"
+                        htmlFor="Price"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {type: 'number'}
+                            { required: true, message: 'It is a required field!' },
+                            { type: 'number' }
                         ]}
                     >
-                        <InputNumber min={0} autoComplete="off"/>
+                        <InputNumber min={0} autoComplete="off" />
                     </Form.Item>
 
                     <Form.Item
                         label="Size"
-                        name="size_id"
-                        htmlFor="size_id"
+                        name="Size"
+                        htmlFor="Size"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
+                            { required: true, message: 'It is a required field!' },
                         ]}
                     >
                         <Select
+                            //mode="multiple"
                             placeholder="Select the size: "
-                            options={categoriesData}
+                            options={sizes.map(item => ({ label: item.label, value: item.value }))}
                         />
                     </Form.Item>
+
                     <Form.Item
                         label="Color"
-                        name="color"
-                        htmlFor="color"
+                        name="Color"
+                        htmlFor="Color"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {min: 3, message: 'Color must have at least 3 symbols!'},
+                            { required: true, message: 'It is a required field!' },
+                            { min: 3, message: 'Color must have at least 3 characters!' },
                         ]}
                     >
-                        <Input autoComplete="off"/>
+                        <Input autoComplete="off" />
                     </Form.Item>
-
 
                     <Form.Item
                         label="Brand"
-                        name="brand_id"
-                        htmlFor="brand_id"
+                        name="BrandId"
+                        htmlFor="BrandId"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
+                            { required: true, message: 'It is a required field!' },
                         ]}
                     >
                         <Select
                             placeholder="Select the brand: "
-                            options={categoriesData}
+                            options={brandsData}
                         />
                     </Form.Item>
 
                     <Form.Item
                         label="Gender"
-                        name="Gender_id"
-                        htmlFor="Gender_id"
+                        name="Gender"
+                        htmlFor="Gender"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
+                            { required: true, message: 'It is a required field!' },
                         ]}
                     >
                         <Select
                             placeholder="Select the Gender: "
-                            options={categoriesData}
+                            options={genders.map(item => ({ label: item.label, value: item.value }))}
                         />
                     </Form.Item>
 
-
                     <Form.Item
                         label="SizeAndFit"
-                        name="sizeAndFit"
-                        htmlFor="sizeAndFit"
+                        name="SizeAndFit"
+                        htmlFor="SizeAndFit"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {min: 10, message: 'sizeAndFit must have at least 10 symbols!'},
+                            { required: true, message: 'It is a required field!' },
+                            { min: 10, message: 'SizeAndFit must have at least 10 characters!' },
                         ]}
                     >
-                        <TextArea/>
+                        <TextArea />
                     </Form.Item>
 
                     <Form.Item
                         label="LookAfterMe"
-                        name="lookAfterMe"
-                        htmlFor="lookAfterMe"
+                        name="LookAfterMe"
+                        htmlFor="LookAfterMe"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {min: 10, message: 'lookAfterMe must have at least 10 symbols!'},
+                            { required: true, message: 'It is a required field!' },
+                            { min: 10, message: 'LookAfterMe must have at least 10 characters!' },
                         ]}
                     >
-                        <TextArea/>
+                        <TextArea />
                     </Form.Item>
-
 
                     <Form.Item
                         label="AboutMe"
-                        name="aboutMe"
-                        htmlFor="aboutMe"
+                        name="AboutMe"
+                        htmlFor="AboutMe"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {min: 10, message: 'aboutMe must have at least 10 symbols!'},
+                            { required: true, message: 'It is a required field!' },
+                            { min: 10, message: 'AboutMe must have at least 10 characters!' },
                         ]}
                     >
-                        <TextArea/>
+                        <TextArea />
                     </Form.Item>
-
 
                     <Form.Item
                         label="Amount"
-                        name="amount"
-                        htmlFor="amount"
+                        name="Amount"
+                        htmlFor="Amount"
                         rules={[
-                            {required: true, message: 'It is a required field!'},
-                            {type: 'number'}
+                            { required: true, message: 'It is a required field!' },
+                            { type: 'number' }
                         ]}
                     >
-                        <InputNumber min={0} autoComplete="off"/>
+                        <InputNumber min={0} autoComplete="off" />
                     </Form.Item>
 
-
                     <Form.Item
-                        name="files"
+                        name="ImageUrls"
                         label="Images"
-                        valuePropName="files"
+                        valuePropName="ImageUrls"
                         getValueFromEvent={(e: UploadChangeParam) => {
                             const image = e?.fileList[0] as IUploadedFile;
                             return image?.originFileObj;
                         }}
-                        rules={[{required: true, message: 'Choose image for category!'}]}
+                        rules={[{ required: true, message: 'Choose image for product!' }]}
                     >
                         <Upload
-                            showUploadList={{showPreviewIcon: false}}
+                            showUploadList={{ showPreviewIcon: false }}
                             beforeUpload={() => false}
                             accept="image/*"
                             listType="picture-card"
                             maxCount={10}
                         >
                             <div>
-                                <PlusOutlined/>
-                                <div style={{marginTop: 8}}>Upload</div>
+                                <PlusOutlined />
+                                <div style={{ marginTop: 8 }}>Upload</div>
                             </div>
                         </Upload>
                     </Form.Item>
-                    <Row style={{display: 'flex', justifyContent: 'center'}}>
-                        <Button style={{margin: 10}} type="primary" htmlType="submit">
+
+                    <Row style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button style={{ margin: 10 }} type="primary" htmlType="submit">
                             Add
                         </Button>
-                        <Button style={{margin: 10}} htmlType="button" onClick={() =>{ navigate('/')}}>
+                        <Button style={{ margin: 10 }} htmlType="button" onClick={() => navigate('/')}>
                             Cancel
                         </Button>
                     </Row>
                 </Form>
             </Row>
         </>
-    )
-}
+    );
+};
 
-export default PostCreatePage;
+export default ProductCreatePage;
--- src/components/types.ts
+++ src/components/types.ts
@@ -3,10 +3,20 @@ export interface ICategoryName {
     name: string,
 }
 export interface IProductCreate {
-    name: string;
-    description: string;
-    category_id: number;
-    files: File[];
+    Name: string;
+    Description: string;
+    CategoryId: number;
+    Price: number;
+    Size:number;
+    Color:string;
+    BrandId:number;
+    SubCategoryId:number;
+    Gender:number;
+    AboutMe:string;
+    LookAfterMe:string;
+    SizeAndFit:string;
+    Amount:number;
+    ImageUrls: File[];
 }
 export interface IUploadedFile {
     lastModified: number;
@@ -18,4 +28,21 @@ export interface IUploadedFile {
     thumbUrl: string;
     type: string;
     uid: string;
+}
+export interface IBrandName {
+    id: number,
+    name: string,
+}
+export interface ISizeName {
+    label: string;
+    value: number;
+}
+export interface IGenderName {
+    label: string;
+    value: number;
+}
+export interface ISubCategoryName {
+    id: number;
+    categoryId: number;
+    name: string;
 }
