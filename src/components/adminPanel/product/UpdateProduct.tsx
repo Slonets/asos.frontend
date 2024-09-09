71efe229fb682db773cfd9d65cc9b80199d2088e
Ilyadelidon <Illadelidon95@gmail.com>
2024-09-09 12:35:44 +0300

    add page update product

--- src/components/adminPanel/product/UpdateProduct.tsx
+++ src/components/adminPanel/product/UpdateProduct.tsx
@@ -5,6 +5,8 @@ import http_common from "../../../http_common";
 import { IBrandName, ICategoryName, IGenderName, IProductCreate, ISizeName } from "../../types.ts";
 import { PlusOutlined } from "@ant-design/icons";
 import { UploadChangeParam, RcFile, UploadFile } from "antd/es/upload/interface";
+import DefaultAdminSideBar from "../default/DefaultAdminSideBar.tsx";
+import ProfileDefaultHeader from "../../UserProfile/default/ProfileDefaultHeader.tsx";
 
 const UpdateProduct = () => {
     const { id } = useParams(); // Get the product ID from the route parameters
@@ -88,88 +90,105 @@ const UpdateProduct = () => {
     const sizesOptions = sizes.map(item => ({ label: item.label, value: item.value }));
 
     return (
-        <div className="edit-product-container">
-            <h2>Edit Product</h2>
-            {product && (
-                <Form
-                    initialValues={product}
-                    onFinish={onFinish}
-                    layout="vertical"
-                    style={{
-                        minWidth: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        padding: 20,
-                    }}
-                >
-                    <Form.Item label="Product Name" name="name">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Description" name="description">
-                        <Input.TextArea />
-                    </Form.Item>
-                    <Form.Item label="Category" name="categoryId">
-                        <Select options={categoriesOptions} />
-                    </Form.Item>
-                    <Form.Item label="Size" name="size">
-                        <Select options={sizesOptions} />
-                    </Form.Item>
-                    <Form.Item label="Color" name="color">
-                        <Input/>
-                    </Form.Item>
-                    <Form.Item label="Brand" name="brandId">
-                        <Select options={brandsOptions} />
-                    </Form.Item>
-                    <Form.Item label="Gender" name="gender">
-                        <Select options={genders.map(gender => ({ label: gender.label, value: gender.value }))} />
-                    </Form.Item>
-                    <Form.Item label="SizeAndFit" name="sizeAndFit">
-                        <Input.TextArea />
-                    </Form.Item>
-                    <Form.Item label="LookAfterMe" name="lookAfterMe">
-                        <Input.TextArea />
-                    </Form.Item>
-                    <Form.Item label="AboutMe" name="aboutMe">
-                        <Input.TextArea />
-                    </Form.Item>
-                    <Form.Item label="Amount" name="amount">
-                        <InputNumber />
-                    </Form.Item>
-                    <Form.Item label="Price" name="price">
-                        <InputNumber />
-                    </Form.Item>
-                    <Form.Item
+        <>
+            <div className="centered-div">
+                <ProfileDefaultHeader/>
+            </div>
 
-                        label="Images"
-                        name="imageUrls"
-                        valuePropName="fileList"
-                        getValueFromEvent={(e: UploadChangeParam<UploadFile<RcFile>>) => {
-                            return e.fileList.map(file => (file.originFileObj as RcFile));
-                        }}
-                        rules={[{ required: false, message: 'Choose images for the product!' }]}
-                    >
-                        <Upload
-                            listType="picture-card"
-                            maxCount={10}
-                            accept="image/*"
-                            beforeUpload={() => false}
-                            showUploadList={{ showPreviewIcon: false }}
+            <div className="main-container">
+
+                <DefaultAdminSideBar/>
+                <div className="content-container">
+                    <div className="div-with-text">
+                        <h2>Edit Product</h2>
+                    </div>
+                    {product && (
+                        <Form
+                            initialValues={product}
+                            onFinish={onFinish}
+                            layout="vertical"
+                            style={{
+                                minWidth: '100%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                padding: 20,
+                            }}
                         >
-                            <div>
-                                <PlusOutlined />
-                                <div style={{ marginTop: 8 }}>Upload</div>
+                            <div className="widthdiv">
+                                <Form.Item label="Product Name" name="name">
+                                    <Input/>
+                                </Form.Item>
+                                <Form.Item label="Description" name="description">
+                                    <Input.TextArea/>
+                                </Form.Item>
+                                <Form.Item label="Category" name="categoryId">
+                                    <Select options={categoriesOptions}/>
+                                </Form.Item>
+                                <Form.Item label="Size" name="size">
+                                    <Select options={sizesOptions}/>
+                                </Form.Item>
+                                <Form.Item label="Color" name="color">
+                                    <Input/>
+                                </Form.Item>
+                                <Form.Item label="Brand" name="brandId">
+                                    <Select options={brandsOptions}/>
+                                </Form.Item>
+                                <Form.Item label="Gender" name="gender">
+                                    <Select
+                                        options={genders.map(gender => ({label: gender.label, value: gender.value}))}/>
+                                </Form.Item>
+                                <Form.Item label="SizeAndFit" name="sizeAndFit">
+                                    <Input.TextArea/>
+                                </Form.Item>
+                                <Form.Item label="LookAfterMe" name="lookAfterMe">
+                                    <Input.TextArea/>
+                                </Form.Item>
+                                <Form.Item label="AboutMe" name="aboutMe">
+                                    <Input.TextArea/>
+                                </Form.Item>
+                                <Form.Item label="Amount" name="amount">
+                                    <InputNumber/>
+                                </Form.Item>
+                                <Form.Item label="Price" name="price">
+                                    <InputNumber/>
+                                </Form.Item>
+                                <div className="image-upload-container">
+                                    <Form.Item
+
+                                        label="Images"
+                                        name="imageUrls"
+                                        valuePropName="fileList"
+                                        getValueFromEvent={(e: UploadChangeParam<UploadFile<RcFile>>) => {
+                                            return e.fileList.map(file => (file.originFileObj as RcFile));
+                                        }}
+                                        rules={[{required: false, message: 'Choose images for the product!'}]}
+                                    >
+                                        <Upload
+                                            listType="picture-card"
+                                            maxCount={10}
+                                            accept="image/*"
+                                            beforeUpload={() => false}
+                                            showUploadList={{showPreviewIcon: false}}
+                                        >
+                                            <div>
+                                                <PlusOutlined/>
+                                                <div style={{marginTop: 8}}>Upload</div>
+                                            </div>
+                                        </Upload>
+                                    </Form.Item>
+                                </div>
                             </div>
-                        </Upload>
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="default" htmlType="submit">
-                            Save Changes
-                        </Button>
-                    </Form.Item>
-                </Form>
-            )}
-        </div>
+                                <Form.Item>
+                                    <Button type="default" htmlType="submit">
+                                        Save Changes
+                                    </Button>
+                                </Form.Item>
+                        </Form>
+                        )}
+                </div>
+            </div>
+        </>
     );
 };
 
