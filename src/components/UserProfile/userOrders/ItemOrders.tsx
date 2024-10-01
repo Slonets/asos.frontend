b2a4574e79e56d38081a8ebf92c3f39e6aef4d0f
Bogdan Slonets <bogdan.slonets@gmail.com>
2024-10-02 01:50:16 +0300

    Add pagination with order

--- src/components/UserProfile/userOrders/ItemOrders.tsx
+++ src/components/UserProfile/userOrders/ItemOrders.tsx
@@ -4,6 +4,24 @@ const ItemOrders=()=>
     return(
         <>
           <h2>Сторінка</h2>
+
+            {/*<div className="orderBox">*/}
+            {/*    <img src="/Clothes/Ragtangel209.png" alt="img" className="Rectangle1"/>*/}
+            {/*    <div className="orderDetails">*/}
+            {/*        <h3>Mixed texture Bow Front Mini Dress</h3>*/}
+            {/*        <p>Brand: Miss Selfridge</p>*/}
+            {/*        <p>Size: UK 12/M</p>*/}
+            {/*        <p>Colour: Blue</p>*/}
+
+            {/*        <div className="priceAndIcon">*/}
+            {/*            <p className="pricetext">$27.99</p>*/}
+            {/*            <div className="iconWrapper">*/}
+            {/*                <FiPackage size={32}/>*/}
+            {/*                <p className="texticon">packed</p>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </>
     )
 };
